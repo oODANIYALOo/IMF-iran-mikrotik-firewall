d77b227e38ca0740bd7b6ed097fe5e87ec6716d5
Daniyel <daniyalx64@gmail.com>
2025-12-08 19:02:13 +0330

    change main.sh for add not complate

--- main.sh
+++ main.sh
@@ -1,7 +1,58 @@
 #!/bin/bash
 
+
+MK_ADD() {
+  ADRESS=$(dialog --form "$MSG" 10 50 4 \
+    "IP: " 1 1 "$IP" 1 12 20 0 \
+    "USER: " 2 1 "$USR" 2 12 20 0 \
+    "PASSWORD: " 3 1 "$PASSWORD" 3 12 20 0 3>&1 1>&2 2>&3)
+  CHECK
+}
+CHECK() {
+  TEMP=$(echo $ADRESS | grep -o " " | wc -l)
+  if [[ $TEMP != 2 ]]; then
+    MSG="Please dont use space AND fill all box"
+    MK_ADD
+  fi
+
+  IP=$(echo $ADRESS | cut -d " " -f1)
+  USR=$(echo $ADRESS | cut -d " " -f2)
+  PASSWORD=$(echo $ADRESS | cut -d " " -f3)
+
+  if ! [[ "$IP" =~ ^([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})$ ]]; then
+    MSG="IP incorrect"
+    MK_ADD
+  elif ! [[ "$USR" =~ ^[a-z,A-Z,0-9]{3,32}$ ]]; then
+    MSG="USER incorrect"
+    MK_ADD
+  elif ! [[ $PASSWORD =~ ^[a-z,A-Z,0-9]{1,50}$ ]]; then
+    MSG="PASSWORD incorrect"
+    MK_ADD
+  fi
+}
+
 ADD() {
-  source ./script/ADD.sh
+	MSG="Enter Mikrotik Info"
+	while true; do
+
+  OP=$(dialog --title "Welcome to IMF" --menu "Select Option" 15 40 4 \
+    1 "ADD SSH Mikrotik" \
+    2 "DELELT SSH Mikrotik" \
+    3 "SHOW all Mikrotik" \
+    4 "back" 3>&1 1>&2 2>&3)
+
+  	case $OP in
+  		1) MK_ADD ;;
+  		2) MK_DEL ;;
+  		3) MK_SHOW ;;
+  		4) return ;;
+  		esac
+	done
+
+
+  ENTRY
+
+  #dialog --title "massage" --msgbox "add successful :)" 6 35
 }
 
 CHECK() {
