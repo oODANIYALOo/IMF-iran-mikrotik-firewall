4710afed9ca4d6c27b96ec12ee12e767675d9322
RezaNikmanesh <rezanikmanesh79@gmail.com>
2025-12-09 16:49:23 +0330

    adding some to view

--- web/configdev/views.py
+++ web/configdev/views.py
@@ -4,4 +4,10 @@ from django.views import View
 
 class Index(View):
     def get(self, request, *args, **kwargs):
+        # display list of devices
         return HttpResponse("Hello, this is index view!")
+
+    def post(self, request, *args, **kwargs):
+        # adding device
+        # deleting device
+        pass
